ba2ee44fdaf560a0c904f7ef177801502e685409
Buutyful <mantelli_git@libero.it>
2024-03-28 01:39:49 +0100

    moved identity services to dc extention

--- Neofilia.Server/Data/ApplicationUser.cs
+++ Neofilia.Server/Data/ApplicationUser.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace Neofilia.Server.Data
-{
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
+namespace Neofilia.Server.Data;
 
+//
+
+public class ApplicationUser : IdentityUser
+{
 }
--- Neofilia.Server/Program.cs
+++ Neofilia.Server/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Neofilia.Server;
 using Neofilia.Server.Components;
 using Neofilia.Server.Components.Account;
 using Neofilia.Server.Data;
@@ -11,29 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityUserAccessor>();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+//Add ef identity
+builder.Services.AddIdentityServices(builder.Configuration);
 
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddIdentityCookies();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
