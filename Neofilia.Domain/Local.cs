aec016b8608e24b5211ab5e1b42e79b2601f7dfd
Buutyful <mantelli_git@libero.it>
2024-03-28 01:29:16 +0100

    made local as the root aggregate, table an entity, still working to figure out the right relationships and architecture, unsecure about using domain events with mediar

--- Neofilia.Domain/Local.cs
+++ Neofilia.Domain/Local.cs
@@ -2,36 +2,35 @@
 using static Neofilia.Domain.Table;
 
 namespace Neofilia.Domain;
-
+//Aggregate root
 //Locale must have many Tables
 //Locale may have many Menus
-//Locale may have an UserManager
+//UserManager may have many Locale
 //how should the global game be implemented?
-//gala gay
-public class Locale
+
+public class Local
 {
-    public int GalAgay { get; set; }
-    public record struct LocaleId(int Id);
-    public LocaleId Id { get; private set; }
-    public Guid? ManagerId { get; private set; }
+    public record struct LocalId(int Id);
+    public LocalId Id { get; private set; } //PK
+    public Guid? ManagerId { get; private set; } //FK: ApplicationUser{ID}, NOT REQUIRED
     public NotEmptyString Name { get; private set; }
     public Adress Adress { get; private set; }
     public DateTimeOffset EventStartsAt { get; private set; }
     public DateTimeOffset EventEndsAt { get; private set; }
 
     //navigation
-    public ICollection<TableId> Tables { get; private set; } = [];
+    public ICollection<Table> Tables { get; private set; } = [];
     public ICollection<MenuId> Menus { get; private set; } = [];
-    public IEnumerable<TableId> TableIds => Tables;
+    public IEnumerable<Table> GetTables => Tables;
     public IEnumerable<MenuId> MenuIds => Menus;
 
-    public Locale(
+    public Local(
         Guid manager,
         string name,
         Adress adress,
         DateTimeOffset eventStartsAt,
         DateTimeOffset eventEndsAt,
-        ICollection<TableId> tableIds,
+        ICollection<Table> tables,
         ICollection<MenuId> menuIds)
     {
         ManagerId = manager;
@@ -41,24 +40,45 @@ public class Locale
             throw new InvalidOperationException("Event needs to start before it ends");
         EventStartsAt = eventStartsAt;
         EventEndsAt = eventEndsAt;
-        Tables = [.. tableIds];
+        Tables = [.. tables];
         Menus = [.. menuIds];
     }
 
-
-    public void AddTable(TableId tableId)
+    //need to add domain events and further validation
+    #region Table
+    public void AddTable(Table table)
     {
-        if (Tables.Contains(tableId))
+        if (Tables.Any(t => t.Equals(table)))
             throw new InvalidOperationException("cant have the same table two times");
-        Tables.Add(tableId);
+        Tables.Add(table);
     }
+    public void RemoveTable(TableId tableId)
+    {
+        var table = Tables.Where(t => t.Id == tableId).First();
+        Tables.Remove(table);
+    }
+    public void AddPartecipantToTable(Guid partecipantId, Table table)
+    {
+        if (Tables.Any(t => t.PartecipantsId
+                  .Any(id => id == partecipantId)))
+            throw new InvalidOperationException("user already partecipating to an other table");
+        table.AddPartecipant(partecipantId);
+    }
+    public void RemovePartecipantFromTable(Guid partecipantId, Table table) => 
+        table.RemovePartecipant(partecipantId);
+    #endregion
+
     public void AddMenu(MenuId menuId)
     {
         if (Menus.Contains(menuId))
             throw new InvalidOperationException("cant have the same menu two times");
         Menus.Add(menuId);
     }
-    public void RemoveTable(TableId table) => Tables.Remove(table);
+    public void RemoveMenu(MenuId menuId)
+    {
+        var menu = Menus.Where(m => m == menuId).First();
+        Menus.Remove(menu);
+    }
     public void ChangeEventStartDate(DateTimeOffset date) => EventStartsAt = date;
     public void ChangeEventEndDate(DateTimeOffset date) => EventEndsAt = date;
 
--- Neofilia.Domain/Table.cs
+++ Neofilia.Domain/Table.cs
@@ -1,15 +1,35 @@
-﻿using static Neofilia.Domain.Locale;
+﻿using static Neofilia.Domain.Local;
+
 
 namespace Neofilia.Domain;
-
-//Table must have a global Locale game
-//Table can have many local games
-//Table can have many partecipants : how to implement this? table should host a lobby?
+//Table is an entity, accessible inside Local aggregate boundry
+//Table must have a global Local game
+//Table may have many local games
+//Table may have many partecipants : how to implement this? table should host a lobby?
 
 public class Table
 {
     public record struct TableId(int Id);
-    public TableId Id { get; private set; }
-    public LocaleId LocaleId { get; private set; }
+    public TableId Id { get; private set; } //PK
+    public LocalId LocalId { get; private set; } //FK: Locals{ID}, REQUIRED
     public int TableNumber { get; private set; } //should this match the id?
+
+
+    //navigation
+    public ICollection<Guid> UsersId { get; private set; } = [];
+    public IEnumerable<Guid> PartecipantsId => UsersId;
+
+    public Table(LocalId localId, int tableNumber) => 
+        (LocalId, TableNumber) = (localId, tableNumber);
+
+    public void AddPartecipant(Guid userId) => UsersId.Add(userId);
+    public void RemovePartecipant(Guid userId) => UsersId.Remove(userId);
+    public bool Equals(Table other) =>
+        other != null && Id.Equals(other.Id);
+    public override bool Equals(object? obj) =>
+        obj is Table table && Equals(table);
+
+    public override int GetHashCode() => Id.GetHashCode();
+   
+
 }
