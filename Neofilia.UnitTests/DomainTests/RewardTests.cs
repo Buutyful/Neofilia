ef8da3813e935935c27aac2a0f5e98de0d5a975d
Buutyful <mantelli_git@libero.it>
2024-03-28 18:24:41 +0100

    reward behavior working tests

--- Neofilia.UnitTests/DomainTests/RewardTests.cs
+++ Neofilia.UnitTests/DomainTests/RewardTests.cs
@@ -13,4 +13,27 @@ public class RewardTests
         // Act and Assert
         Assert.Throws<InvalidOperationException>(() => reward.Redeem());
     }
+
+    [Fact]
+    public void ReedemReward_WhenIsReedemed_ShouldBeSetRedeemedToTrue()
+    {
+        //Arrange
+        var reward = Reward.NewMoneyReward(5.255m);
+        //Act
+        reward.Redeem();
+        //Assert
+        Assert.True(reward.IsRedeemed);
+    }
+
+    [Fact]
+    public void ReedemReward_WhenIsReedemed_ShouldSetRedeemedAtDateCorrectly()
+    {
+        //Arrange
+        var reward = Reward.NewMoneyReward(5.255m);
+        var now = DateTimeOffset.Now;
+        //Act
+        reward.Redeem();
+        //Assert
+        Assert.True(Math.Abs((now - reward.RedeemedAt).TotalSeconds) < 5);
+    }
 }
